5ef26fc8d7102166940b3520f4b274881163d142
snicholas <spadaro.nicholas@gmail.com>
2020-12-24 01:21:43 +0100

    first version

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,19 +12,33 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
-
+  Kp=Kp_;
+  Ki=Ki_;
+  Kd=Kd_;
+  d_error = 0;
+  p_error = 0;
+  i_error = 0;
 }
 
 void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
-
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  double steer = -Kp * p_error - Kd * d_error - Ki * i_error;
+  if (steer < -1) {
+    steer = -1;
+  }
+  if (steer > 1) {
+    steer = 1;
+  }
+  return steer;
 }
